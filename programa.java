32feedc2e7b3bd84da71914baa0fa766357324bb
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-05-07 11:41:15 -0500

    Commit para el nombre

--- programa.java
+++ programa.java
@@ -2,5 +2,6 @@ public class programa{
     public static void main(String[] args){
         System.out.println("Hola mundo");
         System.out.println("Bienvenid@ al curso");
+        System.out.println("Nombre: Jose Alonzo Gordillo Medndoza");
     }
 }
